bb377b171d55f210b6a41f04601365879ef06a9c
Julia Thea <yuliya.boyadjan@gmail.com>
2021-05-15 15:01:12 -0500

    form to sign up users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :articles
+  before_save { self.email = email.downcase }
+
+  has_many :articles, dependent: :destroy
   validates :username, presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 105 }
@@ -10,5 +12,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 25 },
              format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
 end
